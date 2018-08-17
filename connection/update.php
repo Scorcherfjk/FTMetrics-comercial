0c507307d08f8074813b31f41e15803fd1e1d35e
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-17 16:22:10 -0500

    implementando el filtro

--- connection/update.php
+++ connection/update.php
@@ -4,8 +4,14 @@
     require('./consultas.php');
     require('./funciones.php');
 
+    if( $_POST['sPartId'] == "SW" ){
+        $quantity = $_POST['addScrap'] * 1.111;
+    }else{
+        $quantity = $_POST['addScrap'] * 5;
+    }
+
     sqlsrv_begin_transaction( $conn );
-    $query = updateQuery($_POST['lOEEWorkCellId'], $_POST['addScrap']);
+    $query = updateQuery($_POST['lOEEWorkCellId'], $quantity);
     if ( $_POST['addScrap'] <= $_POST['dtotalParts'] ){
         $tt = sqlsrv_query( $conn, $query );
     }
